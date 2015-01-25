1cd325116dbf9f2e55d28bfeed831b2e920a2ee4
Jennings Anderson <jennings.anderson@colorado.edu>
2015-01-25 13:54:23 -0700

    filter work

---
+++ examples/jennings-test-ex.js
@@ -0,0 +1,22 @@
+var csv = require('csv'),
+	fs = require('fs')
+
+var refine = require('../lib')
+
+fs.createReadStream('../data/colorado5.csv')
+	.pipe(csv.parse())
+
+	.pipe(refine.filter(9,"/^G/"))
+//	.pipe(refine.swap(0,8)) //test swap
+
+//	.pipe(refine.lowercase(8))
+
+//	.pipe(refine.uppercase(8))
+
+//	.pipe(refine.remove([10,11,12,13,14,15]))
+
+	// .pipe(refine.replace('Grover town', 'Jeeeun'))
+	// .pipe(refine.replace('Air Force Academy CDP', 'Jennings'))
+	// .pipe(refine.replace('6680', 'Khalid'))
+
+	.pipe(refine.print())

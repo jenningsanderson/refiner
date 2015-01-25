a0238667ff645b01e10a10bb952fac70f8ddd497
qubick <qubick.kim@gmail.com>
2015-01-24 19:50:07 -0700

    added test for remove, replace

--- examples/jeeeun-test-ex.js
+++ examples/jeeeun-test-ex.js
@@ -4,11 +4,18 @@ var csv = require('csv'),
 
 var refine = require('../lib')
 
-fs.createReadStream('colorado.csv')
+fs.createReadStream('../data/colorado5.csv')
 	.pipe(csv.parse())
-//	.pipe(refine.swap(0,2)) //test swap
+//	.pipe(refine.swap(0,8)) //test swap
 
-//	.pipe(refine.lowercase(9))
+//	.pipe(refine.lowercase(8))
 
-	.pipe(refine.uppercase(9))
+//	.pipe(refine.uppercase(8))
+
+//	.pipe(refine.remove([10,11,12,13,14,15]))
+
+	.pipe(refine.replace('Grover town', 'Jeeeun'))
+	.pipe(refine.replace('Air Force Academy CDP', 'Jennings'))
+	.pipe(refine.replace('6680', 'Khalid'))
+
 	.pipe(refine.print())

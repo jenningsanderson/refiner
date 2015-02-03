c55dc88631cad49eb18351639fd1a80178aa8f37
Jennings Anderson <jennings.anderson@colorado.edu>
2015-02-03 14:57:10 -0700

    updated html and vis

--- examples/jennings-v2-ex.js
+++ examples/jennings-v2-ex.js
@@ -4,10 +4,12 @@ var csv = require('csv'),
 var refine = require('../lib/v2')
 var select = refine.select
 var transform = refine.transform
+var viz = refine.viz
 
 fs.createReadStream('../data/colorado5.csv')
 	.pipe(csv.parse())
 	.pipe(refine.start())
     .pipe(select.fields(['NAME'], transform.uppercase()))
     .pipe(refine.end())
-    .pipe(refine.print())
+    .pipe(viz.htmltable())
+    .pipe(refine.print('viz'))
